c03022050e9fe277a6f55d1b4416389b174a7fc6
Neeraj Dalal <admin@nrjdalal.com>
2024-12-09 08:34:47 +0530

    feat: added ni filters

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 - Full responsive and faster than npm
 - 100 results per page (instead of 20 by npm)
 - More results in view without npm's breaking layout
+- Exact match is always on top
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,6 +39,8 @@ const formSchema = z.object({
     'dependent_count',
     'published_at',
   ]),
+  niSort: z.enum(['disabled', 'downloads', 'dependents']),
+  niTitleMatch: z.boolean(),
 })
 
 export default function SuspensePage() {
@@ -66,7 +68,7 @@ function Page() {
     resolver: zodResolver(formSchema),
     defaultValues: {
       q: params.get('q') || '',
-      page: Number(params.get('page')) || 0,
+      page: Number(params.get('page')) || 1,
       perPage: Number(params.get('perPage')) || 100,
       sortBy:
         (params.get('sortBy') as
@@ -75,6 +77,8 @@ function Page() {
           | 'downloads_monthly'
           | 'dependent_count'
           | 'published_at') || '',
+      niSort: 'disabled',
+      niTitleMatch: false,
     },
   })
 
@@ -82,6 +86,7 @@ function Page() {
     mutationFn: async (values: z.infer<typeof formSchema>) => {
       return await npmSearch({
         ...values,
+        page: values.page - 1,
         q: values.q ? values.q : 'next',
         sortBy: values.sortBy ? values.sortBy : 'score',
       })
@@ -104,6 +109,7 @@ function Page() {
     queryFn: async () => {
       return await npmSearch({
         ...(form.getValues() as z.infer<typeof formSchema>),
+        page: form.getValues().page - 1,
         q: form.getValues().q ? form.getValues().q : 'next',
         sortBy: form.getValues().sortBy ? form.getValues().sortBy : 'score',
       })
@@ -112,7 +118,9 @@ function Page() {
 
   return (
     <main className="mx-auto max-w-screen-lg px-3.5 py-5 md:px-10">
-      <h1 className="text-2xl font-bold">npmignore - npm on steroids</h1>
+      <h1 className="text-xl font-bold md:text-2xl">
+        npmignore - npm on steroids
+      </h1>
 
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)}>
@@ -169,142 +177,203 @@ function Page() {
         )}
         {!searching && data?.objects?.length && (
           <div className="min-h-dvh divide-y">
-            <div className="mt-2 flex items-center justify-between py-5">
-              <p className="font-bold">{data.total} pkgs found</p>
+            {/* npm filters */}
+            <div>
+              <div className="mt-2 flex items-center justify-between py-5">
+                <p className="font-bold">{data.total} pkgs found</p>
+                <Select
+                  onValueChange={(value) => {
+                    setSearching(true)
+                    const newParams = new URLSearchParams(params.toString())
+                    newParams.set('sortBy', value)
+                    window.history.pushState(
+                      null,
+                      '',
+                      `?${newParams.toString()}`,
+                    )
+                    form.setValue(
+                      'sortBy',
+                      value as
+                        | 'score'
+                        | 'downloads_weekly'
+                        | 'downloads_monthly'
+                        | 'dependent_count'
+                        | 'published_at',
+                    )
+                    onSubmit(form.getValues())
+                  }}
+                  defaultValue={form.getValues().sortBy}
+                >
+                  <SelectTrigger className="w-48 rounded-sm border-zinc-300 bg-gray-100 text-zinc-700">
+                    <SelectValue placeholder="npm: Default" />
+                  </SelectTrigger>
+                  <SelectContent
+                    className="rounded-none shadow-none"
+                    align="end"
+                  >
+                    <SelectItem value="score">npm: Default</SelectItem>
+                    <SelectItem value="downloads_weekly">
+                      Weekly Downloads
+                    </SelectItem>
+                    <SelectItem value="downloads_monthly">
+                      Monthly Downloads
+                    </SelectItem>
+                    <SelectItem value="dependent_count">
+                      Most Dependents
+                    </SelectItem>
+                    <SelectItem value="published_at">
+                      Recently Published
+                    </SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+
+            {/* custom filter */}
+            <div className="flex items-center space-x-2">
+              <p className="font-semibold">ni filters</p>
               <Select
                 onValueChange={(value) => {
                   setSearching(true)
                   const newParams = new URLSearchParams(params.toString())
-                  newParams.set('sortBy', value)
+                  newParams.set('niSort', value)
                   window.history.pushState(null, '', `?${newParams.toString()}`)
                   form.setValue(
-                    'sortBy',
-                    value as
-                      | 'score'
-                      | 'downloads_weekly'
-                      | 'downloads_monthly'
-                      | 'dependent_count'
-                      | 'published_at',
+                    'niSort',
+                    value as 'disabled' | 'downloads' | 'dependents',
                   )
                   onSubmit(form.getValues())
                 }}
-                defaultValue={form.getValues().sortBy}
+                defaultValue={form.getValues().niSort}
               >
-                <SelectTrigger className="w-48 rounded-sm border-zinc-300 bg-gray-100 text-zinc-700">
-                  <SelectValue placeholder="Sort by: Default" />
+                <SelectTrigger className="w-36 rounded-none border-x border-y-0 shadow-none">
+                  <SelectValue placeholder="Sort by" />
                 </SelectTrigger>
-                <SelectContent className="rounded-none shadow-none" align="end">
-                  <SelectItem value="score">Default</SelectItem>
-                  <SelectItem value="downloads_weekly">
-                    Weekly Downloads
+                <SelectContent
+                  className="rounded-none text-xs shadow-none"
+                  align="center"
+                >
+                  <SelectItem value="disabled text-xs">
+                    Sort: Disabled
                   </SelectItem>
-                  <SelectItem value="downloads_monthly">
-                    Monthly Downloads
-                  </SelectItem>
-                  <SelectItem value="dependent_count">
-                    Most Dependents
-                  </SelectItem>
-                  <SelectItem value="published_at">
-                    Recently Published
-                  </SelectItem>
+                  <SelectItem value="downloads">Downloads</SelectItem>
+                  <SelectItem value="dependents">Dependents</SelectItem>
                 </SelectContent>
               </Select>
             </div>
-            {data?.objects.map((item) => (
-              <div key={item.package.name} className="py-3">
-                <div className="flex flex-wrap items-center justify-between">
-                  <Link
-                    href={`https://npmjs.com/package/${item.package.name}`}
-                    className="flex gap-x-4 font-semibold"
-                    target="_blank"
-                  >
-                    {item.package.name}
-                    {params.get('q') === item.package.name && (
-                      <span className="flex items-center rounded-md bg-purple-200 px-2 text-xs font-normal text-zinc-600">
-                        exact match
+
+            {data?.objects
+              .sort((a, b) => {
+                if (form.getValues().niSort === 'downloads') {
+                  if (form.getValues().sortBy === 'downloads_weekly') {
+                    return (
+                      b.package.downloads.weekly - a.package.downloads.weekly
+                    )
+                  }
+                  return (
+                    b.package.downloads.monthly - a.package.downloads.monthly
+                  )
+                } else if (form.getValues().niSort === 'dependents') {
+                  return b.package.dependents - a.package.dependents
+                } else {
+                  return 0
+                }
+              })
+              .map((item) => (
+                <div key={item.package.name} className="py-3">
+                  <div className="flex flex-wrap items-center justify-between">
+                    <Link
+                      href={`https://npmjs.com/package/${item.package.name}`}
+                      className="flex gap-x-4 font-semibold"
+                      target="_blank"
+                    >
+                      {item.package.name}
+                      {params.get('q') === item.package.name && (
+                        <span className="flex items-center rounded-md bg-purple-200 px-2 text-xs font-normal text-zinc-600">
+                          exact match
+                        </span>
+                      )}
+                    </Link>
+                    <div className="flex items-center justify-end gap-x-1.5 text-sm">
+                      <Download className="size-4" />
+                      <span className="mt-px">
+                        {form.getValues().sortBy === 'downloads_weekly'
+                          ? humanNumbers(item.package.downloads.weekly)
+                          : humanNumbers(item.package.downloads.monthly)}
                       </span>
+                    </div>
+                  </div>
+                  <p className="mt-1 text-sm text-zinc-500">
+                    {item.package.description}
+                  </p>
+
+                  <div className="mt-2 flex flex-wrap gap-2">
+                    {item.package.keywords.slice(0, 14).map((keyword) => (
+                      <span
+                        key={keyword}
+                        className="rounded-md bg-gray-100 px-2 py-1 text-xs text-zinc-600"
+                      >
+                        <Link
+                          href={`/?q=keyword:${keyword}`}
+                          onClick={(e) => {
+                            e.preventDefault()
+                            setSearching(true)
+                            const newParams = new URLSearchParams(
+                              params.toString(),
+                            )
+                            newParams.set('q', `keyword:${keyword}`)
+                            window.history.pushState(
+                              null,
+                              '',
+                              `?${newParams.toString()}`,
+                            )
+                            form.setValue('q', `keyword:${keyword}`)
+                            form.setValue('page', 0)
+                            onSubmit(form.getValues())
+                          }}
+                        >
+                          {keyword}
+                        </Link>
+                      </span>
+                    ))}
+                    {item.package.keywords.length > 13 && (
+                      <Link
+                        href={
+                          'https://www.npmjs.com/package/' + item.package.name
+                        }
+                        className="rounded-md bg-gray-100 px-2 py-1 text-xs text-zinc-600"
+                      >
+                        View {item.package.keywords.length - 13} more
+                      </Link>
                     )}
-                  </Link>
-                  <div className="flex items-center justify-end gap-x-1.5 text-sm">
-                    <Download className="size-4" />
-                    <span className="mt-px">
-                      {form.getValues().sortBy === 'downloads_weekly'
-                        ? humanNumbers(item.package.downloads.weekly)
-                        : humanNumbers(item.package.downloads.monthly)}
-                    </span>
+                  </div>
+
+                  <div className="mr-5 mt-3 flex w-full flex-wrap items-center text-sm text-zinc-500">
+                    <img
+                      className="rounded-sm"
+                      src={
+                        'https://npmjs.com/' +
+                        item.package.publisher.avatars.small
+                      }
+                      alt={item.package.name}
+                      height={24}
+                      width={24}
+                    />
+                    <Link
+                      className="ml-2 font-bold"
+                      href={`https://www.npmjs.com/~${item.package.publisher.name}`}
+                      target="_blank"
+                    >
+                      {item.package.publisher.name}
+                    </Link>
+                    <Dot />
+                    <span>{item.package.version}</span>
+                    <Dot />
+                    <Box className="mr-1 size-4 min-w-4" />
+                    <p>{humanNumbers(item.package.dependents)} dependents</p>
                   </div>
                 </div>
-                <p className="mt-1 text-sm text-zinc-500">
-                  {item.package.description}
-                </p>
-
-                <div className="mt-2 flex flex-wrap gap-2">
-                  {item.package.keywords.slice(0, 14).map((keyword) => (
-                    <span
-                      key={keyword}
-                      className="rounded-md bg-gray-100 px-2 py-1 text-xs text-zinc-600"
-                    >
-                      <Link
-                        href={`/?q=keyword:${keyword}`}
-                        onClick={(e) => {
-                          e.preventDefault()
-                          setSearching(true)
-                          const newParams = new URLSearchParams(
-                            params.toString(),
-                          )
-                          newParams.set('q', `keyword:${keyword}`)
-                          window.history.pushState(
-                            null,
-                            '',
-                            `?${newParams.toString()}`,
-                          )
-                          form.setValue('q', `keyword:${keyword}`)
-                          form.setValue('page', 0)
-                          onSubmit(form.getValues())
-                        }}
-                      >
-                        {keyword}
-                      </Link>
-                    </span>
-                  ))}
-                  {item.package.keywords.length > 13 && (
-                    <Link
-                      href={
-                        'https://www.npmjs.com/package/' + item.package.name
-                      }
-                      className="rounded-md bg-gray-100 px-2 py-1 text-xs text-zinc-600"
-                    >
-                      View {item.package.keywords.length - 13} more
-                    </Link>
-                  )}
-                </div>
-
-                <div className="mr-5 mt-3 flex w-full flex-wrap items-center text-sm text-zinc-500">
-                  <img
-                    className="rounded-sm"
-                    src={
-                      'https://npmjs.com/' +
-                      item.package.publisher.avatars.small
-                    }
-                    alt={item.package.name}
-                    height={24}
-                    width={24}
-                  />
-                  <Link
-                    className="ml-2 font-bold"
-                    href={`https://www.npmjs.com/~${item.package.publisher.name}`}
-                    target="_blank"
-                  >
-                    {item.package.publisher.name}
-                  </Link>
-                  <Dot />
-                  <span>{item.package.version}</span>
-                  <Dot />
-                  <Box className="mr-1 size-4 min-w-4" />
-                  <p>{humanNumbers(item.package.dependents)} dependents</p>
-                </div>
-              </div>
-            ))}
+              ))}
           </div>
         )}
       </div>
