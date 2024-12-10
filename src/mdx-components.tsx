307f17876db9aeb29d265d91ad43053db63fcebe
Neeraj Dalal <admin@nrjdalal.com>
2024-12-10 22:25:27 +0530

    feat: alpha version

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,7 +1,21 @@
 import type { MDXComponents } from 'mdx/types'
+import Link from 'next/link'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
+    a: (props) => {
+      const { href, ...rest } = props
+
+      return (
+        <Link
+          href={href}
+          target={href.startsWith('http') ? '_blank' : undefined}
+          {...rest}
+        >
+          {props.children}
+        </Link>
+      )
+    },
   }
 }
