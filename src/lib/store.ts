3874760774c5182ea7fde830502640cc54a097bf
Neeraj Dalal <admin@nrjdalal.com>
2024-12-10 22:48:32 +0530

    feat: alpha version

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -10,6 +10,12 @@ interface SearchParamsType {
 export const SearchParams = atom<SearchParamsType>({})
 export const Searching = atom<boolean>(false)
 export const SearchResults = atom<NpmSearchResult>({
+  formData: {
+    q: { value: '' },
+    page: { value: 0 },
+    perPage: { value: 0 },
+    sortBy: { value: 'score' },
+  },
   total: 0,
   objects: [],
 })
